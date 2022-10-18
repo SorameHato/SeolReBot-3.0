b4e301a78d535bad7d01b5a612d73588c14ae14e
Jeon Hojin <ghwls030306@naver.com>
2022-10-19 03:03:46 +0900

    모듈화 (일단) 취소, 열번분석 embed로 출력하게 변경, 도박기능 구현 위한 테스트
    모듈화는 나중의 내가 해주겠지.. cogi로 어떻게 하면 된다던데
    도박기능 구현을 위한 id 얻어오는 테스트
    열번분석 열번이 정상적일 경우 embed로 출력

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 import random
 import sys
 from datetime import datetime as dt
-import Module.SeolReLib as SRLib
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,7 +15,7 @@ bot = commands.Bot(command_prefix='.설레 ', intents=intents)
 str.startswith 메서드를 사용할 필요가 없습니다.
 '''
 
-ver = "3.0_2022101602 rev 0.0 build 15"
+ver = "3.0_2022101901 rev 0.0 build 17"
 
 def 확률처리(arg):
     # arg문 정제(?)
@@ -113,9 +112,6 @@ async def 야식메뉴추천(ctx):
 async def 정보(ctx):
     await ctx.send("설레봇의 정보입니다!\n> 버전 : "+ver+"\n> 기반 : 2.5_2021021703 rev 6.3 build 14 (2021년 2월 18일 1시 0분 45초)\n> DB1 : PJU:K:C:B:2021103105 (2021년 10월 31일 4시 3분 51초)\n> DB2 : b102dff1ef5ddf5e3e9d7a4028656a90aa921252 (2022년 9월 21일 2시 47분 0초)\n> 설레봇을 개발한 사람 : 하늘토끼(ghwls030306@s-r.ze.am)\n> 설레봇이 시작된 시간 : " + LoadedTime)
 
-@bot.command()
-async def 열번분석(ctx,*,text):
-    await ctx.send(SRLib.열번분류(text))
 
 @bot.command()
 async def 시간(ctx):
@@ -131,6 +127,10 @@ async def 도박(ctx,*,text):
         await ctx.send("아직 데이터 확인 부분의 코드가 완성되지 않았어요. 하지만 벌써부터 회생을 하려는 걸 보니 당신은 도박 중독자이신 것 같네요.\n한국도박문제예방치유원에서 운영하는 도박문제 전화상담 헬프라인은 국번 없이 1336번이에요. 한 번 전화해서 도움을 받아보시는 게 어떤가요?")
 
 @bot.command()
+async def 테스트(ctx):
+    await ctx.send('테스트 결과입니다!\n> 보낸 사람 : {0}\n> 보낸 사람의 닉네임 : {1}\n> 보낸 사람의 ID : {2}\n> 보낸 사람을 멘션할 경우 : <@{2}>'.format(ctx.message.author, ctx.message.author.name, ctx.message.author.id))
+
+@bot.command()
 async def 도움말(ctx,*,text='0'):
     num = text.split(' ')[0]
     if(num == '1' or num == '0' or num == ''):
@@ -141,6 +141,167 @@ async def 도움말(ctx,*,text='0'):
         await ctx.send('설레봇의 명령어 목록입니다!(3/3)\n이 페이지는 도박 명령어만 다루는 특별 페이지에요. 아쉽게도 도박 기능은 아직 구현 중이에요. 데이터베이스를 다루는 방법을 배우고 있고, 최대한 재미있게 만드는 방법을 고민하고 있거든요. 많은 기대 부탁드릴게요!\n.설레 도박 회생\n> 돈을 모두 탕진하신 경우, 1주일에 한 번 회생할 수 있어요.\n> 설레봇은 개인회생을 해도 딱히 불이익이 없긴 하지만, 돈이 만원으로 초기화되고 추후 여러가지 불이익을 구현할 예정이니 남용은 하지 말아주세요!')
     else:
         await ctx.send('아직 존재하지 않는 페이지를 입력하셨어요. 설레봇은 2020년 5월에 만들어진 따끈따끈한 봇이라 아직 기능이 그렇게 많지는 않답니다. 앞으로 더 발전할 설레봇을 기대해주세요!')
+
+@bot.command()
+async def 열번분석(ctx,*,text):
+    arg=text
+    if arg[0:2]=='SM' and len(arg) == 9: #설빈레피딕스 도시철도 열차
+        종류=0 #기본값
+        노선명='존재하지 않는 노선입니다.'                        #노선/등급/라이너 정보가 오류로 인해
+        등급명='존재하지 않는 등급입니다.'                        #지정되지 않을 경우를 대비해
+        라이너명='존재하지 않는 라이너 열차입니다.'               #미리 값을 지정해 놓음
+        라이너종류='존재하지 않는 열번이므로 알 수 없습니다.'     #이러면 오류는 안 뜨겠지
+        열차번호=1048576                                          #얘는 사실 처리가 귀찮아서 콜록
+        
+        등급=arg[2:4] #SM'OL'21031
+        등급분류=[['OR',1,'일반'],['SW',1,'일반쾌속'],['PW',1,'선별쾌속'],['RP',1,'일반급행'],['CM',1,'통근급행'],['LM',1,'쾌속급행'],['XC',1,'특별급행'],['EX',1,'고속급행'],['LN',2,'라이너']] #등급 분류 데이터
+        for i in range(len(등급분류)):
+            if(등급==등급분류[i][0]):
+                종류=등급분류[i][1] #도시철도 열차는 1, 라이너는 2
+                등급명=등급분류[i][2] #등급명
+        if(종류==0): #등급 분류에 실패한 경우
+            await ctx.send('등급이 올바르지 않아요! V03.01 개편 이후의 열번을 입력해주세요.')
+        elif(종류==1): #도시철도
+            try:
+                노선 = int(arg[4:6])
+            except(ValueError): #광역/로컬선
+                노선 = arg[4:6]
+                switcher={'ES':'동해시사이드선','WF':'서해환상선','SL':'남해선','NE':'북해쾌속선','NL':'북해로컬선','NS':'북해연안선','EY':'엔유선','E1':'Ex1호선','EC':'동청산선','YR':'용림선','YS':'예산세현선','DS':'대산설원선'}
+                try:
+                    노선명=switcher.get(노선,'존재하지 않는 노선입니다.')
+                except(UnboundLocalError):
+                    노선명='존재하지 않는 노선입니다.'
+            else:#도시철도선
+                if(노선>0 and 노선<=20):
+                    노선명 = '용해{}호선'.format(노선)
+                elif(노선==93 or 노선==94 or 노선==98 or 노선==99 or 노선==0 or 노선>=100):
+                    노선명 = '존재하지 않는 노선입니다.'
+                elif(노선==30 or 노선==31):
+                    노선명 = 'CQ{}호선'.format(노선-20)
+                elif(노선<=40):
+                    노선명 = '유하{}호선'.format(노선-20)
+                elif(노선<=60):
+                    노선명 = '양서{}호선'.format(노선-40)
+                elif(노선<=80):
+                    노선명 = '가람{}호선'.format(노선-60)
+                elif(노선<=89):
+                    노선명 = '동명{}호선'.format(노선-80)
+                else:
+                    switcher = {90:'시온종주선',91:'능곡청안선',92:'남해하랑선',95:'종단 급행 노선',96:'강서청양선',97:'유현숲길선'}
+                    노선명 = '시온 어반 네트워크 ' + switcher.get(노선,'존재하지 않는 노선')
+            embed = discord.Embed(title='{} 열번의 분석 결과에요!'.format(arg),color=0x04ccff)
+            embed.add_field(name="노선명",value=노선명, inline=True)
+            embed.add_field(name="등급명",value=등급명, inline=True)
+            embed.set_footer(text='설빈레피딕스 도시철도 열차 열번 분석 결과')
+            await ctx.send(embed=embed)
+        elif(종류==2): #라이너
+            try:
+                열번 = int(arg[4:9])
+            except(ValueError):
+                await ctx.send('라이너 열차의 경우, SMLN 뒤의 다섯자리는 전부 숫자로 변경되었어요. 다시 확인하시고 입력해주세요.')
+            else:
+                if(열번<=9999 or 열번>=40000):
+                    await ctx.send('라이너 열차의 열번이 올바르지 않아요.')
+                else:
+                    if(열번<=20000):
+                        라이너종류='통근 라이너'
+                        if(열번>=10000 and 열번<=10999):
+                            라이너명='구형 라이너 (남아있는 정보가 없어 정확히 어떤 라이너인지는 알 수 없어요.)'
+                            열차번호=format(열번%10000,'04')
+                        elif(열번>=11000 and 열번<=11999):
+                            라이너명='Dawn Express'
+                            열차번호=format(열번%10000,'04')
+                        elif(열번>=12001 and 열번<=12060):
+                            라이너명='Glowing Express'
+                            열차번호=format(열번%1000,'03')
+                        elif(열번>=13001 and 열번<=13080):
+                            라이너명='Sunset Liner'
+                            열차번호=format(열번%1000,'03')
+                        elif(열번>=13081 and 열번<=13160):
+                            라이너명='Satellite Network'
+                            열차번호=format(열번%1000,'03')
+                        elif(열번>=13161 and 열번<=13240):
+                            라이너명='RiverShore Express'
+                            열차번호=format(열번%1000,'03')
+                        elif(열번>=13501 and 열번<=13620):
+                            라이너명='Urban Liner(능곡청안선 · 남해하랑선 계통)'
+                            열차번호=format(열번%1000-500,'03')
+                        elif(열번>=13621 and 열번<=13740):
+                            라이너명='Urban Liner(강서청양선 · 유현숲길선 계통)'
+                            열차번호=format(열번%1000-620,'03')
+                        elif(열번>=13741 and 열번<=13770):
+                            라이너명='Urban Liner(능곡청안선 · 남해하랑선 계통 - 우회)\n학익대-유화 : 6호선'
+                            열차번호=format(열번%1000-540,'03')
+                        elif(열번>=13771 and 열번<=13790):
+                            라이너명='Urban Liner(능곡청안선 · 남해하랑선 계통 - 우회)\n학익대-단비구청 : 6호선, 단비구청-에리 : 12호선'
+                            열차번호=format(열번%1000-520,'03')
+                        elif(열번>=13791 and 열번<=13810):
+                            라이너명='Urban Liner(능곡청안선 · 남해하랑선 계통 - 우회)\n여진구청-유현 : 2호선, 유현-벼리시청 : CQ10호선, 벼리시청-단비구청 : 12호선, 단비구청-유화 : 6호선'
+                            열차번호=format(열번%1000-490,'03')
+                        else:
+                            라이너명='열번이 올바르지 않거나 아직 DB에 추가되지 않은 라이너에요.'
+                    elif(열번<=29999):
+                        라이너종류='관광 라이너'
+                        if(열번>=20001 and 열번<=20080):
+                            라이너명='쿠로카제'
+                            열차번호=format(열번%1000,'03')
+                        elif(열번>=20081 and 열번<=20120):
+                            라이너명='아오조라'
+                            열차번호=format(열번%1000,'03')
+                        elif(열번>=21001 and 열번<=21030):
+                            라이너명='유키카제'
+                            열차번호=열번%10000
+                        elif(열번>=21031 and 열번<=21070):
+                            라이너명='아메카제'
+                            열차번호=열번%10000
+                        elif(열번>=21071 and 열번<=21130):
+                            라이너명='호시유메'
+                            열차번호=열번%10000
+                        else:
+                            라이너명='열번이 올바르지 않거나 아직 DB에 추가되지 않은 라이너에요.\n> 아니면 V03.01 개편 때 통근 라이너나 여객 라이너로 바뀐 라이너일 수도 있으니, 다시 한 번 확인해주세요!'
+                    else:
+                        라이너종류='여객 라이너'
+                        if(열번>=31000 and 열번<=31299):
+                            라이너명='하늘특별시발 시온특별시행 CassioPeia Traveler'
+                            열차번호=format(열번%1000,'03')
+                        elif(열번>=31300 and 열번<=31499):
+                            라이너명='하늘특별시발 송월광역시행 CassioPeia Traveler'
+                            열차번호=열번%1000
+                        elif(열번>=31500 and 열번<=31749):
+                            라이너명='하늘특별시발 시온특별시행 CassioPeia Commuter'
+                            열차번호=열번%1000
+                        elif(열번>=31750 and 열번<=31900):
+                            라이너명='하늘특별시발 송월광역시행 CassioPeia Commuter'
+                            열차번호=열번%1000
+                        elif(열번>=31901 and 열번<=31999):
+                            라이너명='CassioPeia 임시열차'
+                            열차번호=열번%1000
+                        elif(열번>=32001 and 열번<=32060):
+                            라이너명='Forest Liner'
+                            열차번호=format(열번%1000,'03')
+                        elif(열번>=32061 and 열번<=32110):
+                            라이너명='StarLight Express'
+                            열차번호=format(열번%1000,'03')
+                        elif(열번>=34000 and 열번<=34999):
+                            라이너명='Twilight Express NightLiner'
+                            열차번호='N{}'.format(열번%10000)
+                        else:
+                            라이너명='열번이 올바르지 않거나 아직 DB에 추가되지 않은 라이너에요.'
+                    embed = discord.Embed(title='{} 열번의 분석 결과에요!'.format(arg),color=0x04ccff)
+                    embed.add_field(name="라이너 종류",value=라이너종류, inline=False)
+                    if(열차번호 != 1048576):
+                        embed.add_field(name="라이너 이름",value=라이너명, inline=True)
+                        embed.add_field(name="열차번호",value='제 {}열차'.format(열차번호), inline=True)
+                    else:
+                        embed.add_field(name="라이너 이름",value=라이너명, inline=False)
+                    embed.set_footer(text='설빈레피딕스 라이너 열차 열번 분석 결과')
+                    await ctx.send(embed=embed)
+        else:
+            await ctx.send('데이터 처리 중 오류가 발생했어요. 다시 시도하시거나 열번을 다시 확인해주세요!')
+    elif arg[0:2]=='SF': #설빈레피딕스 화물열차
+        await ctx.send('아직 화물열차는 구현되지 않았어요. 빠른 시일 내에 구현할게요!')
+    else: #설레 열차가 아닌 경우
+        await ctx.send('열번이 올바르지 않거나, 설빈레피딕스의 열번이 아니에요. 다시 확인하시고 입력해주세요!')
 
 @bot.command()
 async def 셧다운(ctx):
