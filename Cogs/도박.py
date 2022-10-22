f6db74f001a5cb294ec75d0ef06c591d0f0c919d
Jeon Hojin <ghwls030306@naver.com>
2022-10-23 00:58:31 +0900

    도박 데이터 조회, 변경부분 완성
    코드가 너무 중복되는 것 같지만 넘어가자 (?)

--- Cogs/도박.py
+++ Cogs/도박.py
@@ -14,6 +14,9 @@ class _도박(bot.Cog):
     def __init__(self,bot):
         self.bot = bot
     
+    '''해당 유저의 잔고를 조회하는 명령어
+    input : uid (ctx.message.author.id 로 얻은 해당 유저의 id)
+    output : 해당 유저의 잔액(단, 해당 유저의 데이터가 없는 경우 -1)'''
     def 조회(self,uid):
         exist = False
         with open('data.csv','r',newline='',encoding='utf-8') as a:
@@ -24,12 +27,23 @@ class _도박(bot.Cog):
                     return line[1]
         # .설레 도박 조회 명령어에서는 -1값이 반환되면 자동으로 db추가를 진행하는 기능을 추가해야 함
         # 이 외의 경우에는 그냥 데이터가 없다고만 출력
-        if exist = False:
+        if exist == False:
             return -1
     
+    '''해당 유저의 잔고를 변경하는 명령어
+    input : uid (ctx.message.author.id 로 얻은 해당 유저의 id)
+    amount (얼마나 변경할 것인지, 주의해야 할 점은 잔액을 인출하는 경우 음수로 입력해야 함, 그리고 절댓값을 입력하는 게 아님. 26만원에서 25만 5천원으로 변경하고 싶으면 255000이 아닌 -5000으로 입력해야 함)
+    output : 인출/입금 후 잔액 (단, 해당 유저의 데이터가 없는 경우 -1)'''
     def 변경(self,uid,amount):
+        db = list()
         with open('data.csv','r',newline='',encoding='utf-8') as a:
-            db = pickle.load(a)
+            reader = csv.reader(a)
+            for line in reader:
+                if line[0] == uid:
+                    if line[1] >= amount
+                db.append(line)
+        with open('data.csv','w',newline='',encoding='utf-8') as a:
+            writer = csv.writer(a)
 
 async def setup(bot):
     await bot.add_cog(_도박(bot))
