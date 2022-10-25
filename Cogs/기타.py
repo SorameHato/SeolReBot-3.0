b104846516a1830d8671547e365bceacd55f9a12
Jeon Hojin <ghwls030306@naver.com>
2022-10-26 01:39:46 +0900

    정보 임베드화

--- Cogs/기타.py
+++ Cogs/기타.py
@@ -16,7 +16,14 @@ class 기타(bot.Cog):
     
     @bot.command()
     async def 정보(self, ctx):
-        await ctx.send(f"설레봇의 정보입니다!\n> 버전 : {self.bot.srver}\n> 기반 : 2.5_2021021703 rev 6.3 build 14 (2021년 2월 18일 1시 0분 45초)\n> DB1 : PJU:K:C:B:2021103105 (2021년 10월 31일 4시 3분 51초)\n> DB2 : b102dff1ef5ddf5e3e9d7a4028656a90aa921252 (2022년 9월 21일 2시 47분 0초)\n> 설레봇을 개발한 사람 : 하늘토끼(ghwls030306@s-r.ze.am)\n> 설레봇이 시작된 시간 : {self.bot.LoadedTime}")
+        embed = discord.Embed(title='설레봇의 정보입니다!',color=0x04ccff)
+        embed.add_field(name='버전',value=self.bot.srver,inline = False)
+        embed.add_field(name='기반이 된 버전',value='코드 : 2.5_2021021703 rev 6.3 build 14 (2021년 2월 18일 1시 0분 45초)\nDB1 : PJU:K:C:B:2021103105 (2021년 10월 31일 4시 3분 51초)\nDB2 : b102dff1ef5ddf5e3e9d7a4028656a90aa921252 (2022년 9월 21일 2시 47분)',inline = False)
+        embed.add_field(name='개발자',value='하늘토끼(ghwls030306@s-r.ze.am)',inline = False)
+        embed.add_field(name='깃허브 링크',value='https://github.com/SkyRabbITs/SeolReBot-3.0',inline = False)
+        embed.add_field(name='설레봇이 시작된 시간',value=self.bot.LoadedTime,inline = False)
+        embed.set_footer(text='설레봇 버전 {}'.format(self.bot.srver))
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(기타(bot))
