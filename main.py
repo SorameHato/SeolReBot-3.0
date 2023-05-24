e50323ced8370455c3b36bbdfb045d5c726f5703
Jeon Hojin <36477282+SorameHato@users.noreply.github.com>
2023-05-24 16:51:37 +0900

    아로나봇에 맞게 main.py 업데이트

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime as dt
 import os
 import sys
 bot = discord.Bot()
-ver = "3.2_2023051100 rev 2.33-ReBirth 01 build 198"
+ver = "3.2_2023051100 rev 2.33-ReBirth 01 build 198" + "AronaBot Branch"
 guild_ids = [
     1030056186915082262 #테스트용 서버
     #959065568135241728, 단비냐아 서버
@@ -21,14 +21,14 @@ async def on_ready():
     LoadedTime = str(dt.now().strftime("%Y년 %m월 %d일 %H시 %M분 %S.%f"))[:-3]+"초"
     bot.LoadedTime = LoadedTime
     print('┌────────────────────────────────────────────────────────┐\n│   {name}(#{id})으로 로그인되었습니다.   │\n│ 봇이 시작된 시각 : {LoadedTime} │\n└────────────────────────────────────────────────────────┘'.format(name=bot.user.name,id=bot.user.id,LoadedTime=LoadedTime))
-    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name='/도움말 | 설레봇 v3.2'))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name='/도움말 | 아로나봇 v3.2'))
 
 @bot.event
 async def on_application_command_error(ctx, error):
     embed = discord.Embed(title='자세한 내용',description=error,color=0xfae5fa)
     embed.add_field(name="보낸 분",value=ctx.author,inline=False)
     embed.add_field(name="보낸 내용",value=ctx.message,inline=False)
-    embed.set_footer(text=f'설레봇 버전 {ver}')
+    embed.set_footer(text=f'아로나봇 버전 {ver}')
     await ctx.respond('오류가 발생했어요!',embed=embed)
     raise error
 
