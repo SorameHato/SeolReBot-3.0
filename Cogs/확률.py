03daea72253696f99836980a231b217bda2129cc
Jeon Hojin <ghwls030306@naver.com>
2023-06-23 11:42:09 +0900

    함수이름 변경

--- Cogs/확률.py
+++ Cogs/확률.py
@@ -26,8 +26,8 @@ class _prob(commands.Cog): #prob은 probability의 약자
             arg = arg[:-1]
         return(f"{arg} 확률은 {random.randint(0,100)}%이에요!")
     
-    @commands.slash_command(guild_ids = guild_ids, description="무언가가 일어날 확률이 궁금하시다고요? 유설레가 계산해드릴게요!")
-    async def 확률(self, ctx,text:discord.Option(str,'어떤 것의 확률을 계산할 지 입력해주세요. (예시 : 하토가 귀엽지 않을, 하토가 귀엽지 않을 확률, 하토가 내일 오락을 갈 가능성)',name='문장')):
+    @commands.slash_command(name='확률',guild_ids = guild_ids, description="무언가가 일어날 확률이 궁금하시다고요? 유설레가 계산해드릴게요!")
+    async def prob(self, ctx,text:discord.Option(str,'어떤 것의 확률을 계산할 지 입력해주세요. (예시 : 하토가 귀엽지 않을, 하토가 귀엽지 않을 확률, 하토가 내일 오락을 갈 가능성)',name='문장')):
         await ctx.respond(self.probCalc(text))
 
 def setup(bot):
