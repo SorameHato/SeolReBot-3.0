6e8b55a00eed46d8210e31d2c7137f4507dc1cda
Jeon Hojin <ghwls030306@naver.com>
2023-06-23 11:40:37 +0900

    확률함수 다시 짬

--- Cogs/확률.py
+++ Cogs/확률.py
@@ -1,32 +1,30 @@
 # coding: utf-8
-import discord
+import discord, os, sys
 from discord.ext import commands
-import random
 global guild_ids
-import os
-import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))) #상위폴더의 파일을 import할 수 있게 해주는 거
 from main import guild_ids
+import random
 
-class _확률(commands.Cog):
+class _prob(commands.Cog): #prob은 probability의 약자
     def __init__(self,bot):
         self.bot = bot
     
-    def 확률처리(self,arg):
-        # arg문 정제(?)
-        if(arg[len(arg)-2:len(arg)] == '확률'):
-            arg = arg[0:len(arg)-2]
-        if(arg[len(arg)-3:len(arg)] == '가능성'):
-            arg = arg[0:len(arg)-3]
-        if(arg[len(arg)-2:len(arg)] == '비율'):
-            arg = arg[0:len(arg)-2]
-        if(arg[len(arg)-1:len(arg)] == ' '):
-            arg = arg[0:len(arg)-1]
+    def probCalc(self,arg):
+        #arg문 정제(?)
+        if(arg[-2:] == '확률'):
+            arg = arg[:-2]
+        if(arg[-3:] == '가능성'):
+            arg = arg[:-3]
+        if(arg[-2:] == '비율'):
+            arg = arg[:-2]
+        if(arg[-1:] == ' '):
+            arg = arg[:-1]
         return(f"{arg} 확률은 {random.randint(0,100)}%이에요!")
     
-    @commands.slash_command(guild_ids = guild_ids, description="설레봇이 확률을 계산해줘요!")
-    async def 확률(self, ctx,text:discord.Option(str,'어떤 것의 확률을 계산할 지 입력해주세요. (예시 : 판치가 여자일, 판치가 여자일 확률, 하토가 내일 오락을 갈 가능성)',name='문장')):
-        await ctx.respond(self.확률처리(text))
+    @commands.slash_command(guild_ids = guild_ids, description="무언가가 일어날 확률이 궁금하시다고요? 유설레가 계산해드릴게요!")
+    async def 확률(self, ctx,text:discord.Option(str,'어떤 것의 확률을 계산할 지 입력해주세요. (예시 : 하토가 귀엽지 않을, 하토가 귀엽지 않을 확률, 하토가 내일 오락을 갈 가능성)',name='문장')):
+        await ctx.respond(self.probCalc(text))
 
 def setup(bot):
-    bot.add_cog(_확률(bot))
+    bot.add_cog(_prob(bot))
