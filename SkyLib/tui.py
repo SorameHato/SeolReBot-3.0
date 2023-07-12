8734683c6d4619f0acce0cf9174bf35cd4c05d20
Jeon Hojin <ghwls030306@naver.com>
2023-07-13 01:21:38 +0900

    add : SkyLIb 업데이트 반영

--- SkyLib/tui.py
+++ SkyLib/tui.py
@@ -67,3 +67,28 @@ def fixedWidth(text:str,length:int,array:int=0):
                 return (' '*(halfLen+1)) + text + (' '*halfLen)
     else:
         return text
+
+def fixedWidthAlt(text:str,length:int,array:int=0):
+    '''입력받은 텍스트를 왼쪽 정렬, 가운데 정렬, 또는 오른쪽 정렬로 해서 고정된 넓이로 출력하는 함수
+    예를 들어 길이 11로 가나다라라는 텍스트를 출력하는 경우
+    array : 0 (왼쪽 정렬)   '가나다라   '
+    array : 1 (오른쪽 정렬) '   가나다라'
+    fullWidth를 적용하긴 조금 그렇고 각 열의 데이터의 글자수가 일정한 열 3개 이상의 표를 만들 때 유용
+    만약 text의 길이가 length보다 길면 그냥 그 text 자체를 추가 공백 없이 return 함'''
+    # text가 str형이 아닌 경우를 대비하기 위한 전처리
+    text = str(text)
+    # 먼저 text의 길이를 체크
+    txtLen = __charLen__(text)
+    # 그 다음 text의 길이가 length보다 긴지 아닌지 체크
+    if txtLen >= length:
+        return text
+    # 그 다음 array의 종류를 체크, 0이나 2면 단순히 txtLen - length 만큼의 공백을 붙인 str열을 return
+    # 1이나 3이면 txtLen - length가 짝수면 단순히 양쪽에 int((txtLen - length) / 2) 만큼의 공백을 붙여서 출력하고
+    # 만약 홀수라면 array가 1이면 왼쪽은 int((txtLen - length) / 2) 오른쪽은 int((txtLen - length) / 2) + 1
+    #              array가 3이면 왼쪽은 int((txtLen - length) / 2) + 1 오른쪽은 int((txtLen - length) / 2)
+    if array == 0:
+        return text + ('　'*int((length - txtLen)/2)) + (' '*int((length - txtLen)%2))
+    elif array == 1:
+        return ('　'*int((length - txtLen)/2)) + (' '*int((length - txtLen)%2)) + text
+    else:
+        return text
